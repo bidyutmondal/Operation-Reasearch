959e345048fea21c48f9a80576f2783253141d20
Bidyut Mondal <61779212+bidyutmondal@users.noreply.github.com>
2021-02-25 23:20:53 +0530

    Add files via upload

---
+++ revSimplex.py
@@ -0,0 +1,214 @@
+#Bidyut Mondal
+#18MA20010
+#OR LAB 7 Revised Simplex
+
+import numpy as np
+M = 1e5
+def read():
+    global var, n, mat, count, obj
+    var = int(input("Enter the number of variables: "))
+    n = int(input("Enter the number of equations: "))
+    mat = [[]for i in range(n)]
+    nmat = [[]for i in range(n)]
+    cnst = []
+    arr = []
+    
+    for i in range(var):
+        arr.append(0)
+        
+    count = var
+    
+    inq = []
+    for i in range(n):
+        for j in range(var):
+            nmat[i].append(float(input(F'{"X["}{i}{","}{j}{ "] : "}')))
+        
+        inq.append((input(F'{"Enter the inquality(<=, >=, ==) in cnsttraint" } {i+1} {" : "}')))
+        
+        cnst.append(float(input(F'{"B["}{i}{ "] : "}')))
+    
+    print("\n")
+    for i in range(n):
+        for j in nmat[i]:
+            mat[i].append(j)
+    
+    count = var-1
+    obj = []
+    for j in range(var):
+        obj.append(0)
+        
+    for i in range(n):
+        if(inq[i] == "<="):
+            count += 1
+            obj.append(0)
+            arr.append(0)
+            for j in range(n):
+                if(i != j):
+                    mat[j].append(0)
+            mat[i].append(1)
+        elif(inq[i] == ">="):
+            count += 2
+            obj.append(0)
+            arr.append(0)
+            obj.append(-1*M)
+            arr.append(1)
+            for j in range(n):
+                if(i!=j):
+                    mat[j].append(0)
+                    mat[j].append(0)
+            mat[i].append(-1)
+            mat[i].append(1)
+        elif(inq[i] == "=="):
+            count += 1
+            obj.append(-1*M)
+            arr.append(1)
+            for j in range(n):
+                if(i != j):
+                    mat[j].append(0)
+            mat[i].append(1)
+    
+    count += 1
+    for i in range(n):
+        mat[i].append(cnst[i])
+    
+    print("Coefficient Matrix : ")
+    for i in range(n): print(mat[i])
+    
+    s = int(input("Choose - 1.Minimize  2.Maximize : "))
+    print("\n")
+    print("Enter the coefficient of objective function:")
+    for j in range(var):
+        coe = float(input(F'{"X"}{j+1}{ " : "}'))
+        if(s == 1):
+            obj[j] = -1*coe
+        else:
+            obj[j] = coe
+    
+    print("\n Objective function : ", end ='')
+    print(obj)
+
+
+def revSimplex():
+    global basis, basic, b, n_b
+    basis = []
+    b = [[]for i in range(n)]
+    for i in range(n):
+        for j in range(var,count):
+            if(mat[i][j] == 1):
+                basis.append(j)
+                for k in range(n):
+                    b[i].append(mat[k][j])
+    
+    n_b = [[]for i in range(n)]
+    for j in range(n):
+        for k in range(n):
+            if(j == k):
+                n_b[j].append(1)
+            elif(j!=k):
+                n_b[j].append(0)
+    
+    basic = []
+    for j in range(count):
+        basic.append(0)
+    for j in range(n):
+        basic[basis[j]] = 1
+
+
+if __name__ == '__main__':
+    read()
+    revSimplex()
+    itr = 0
+    print("\n------------------------------------------------------------------")
+    while(1):
+        print("------------------------------------------------------------------\n")
+        print("Interation : " , itr+1, "\n")
+        cb = [obj[basis[i]]for i in range(n)]
+        print("Basic variables : ", end='')
+        for j in range(n):
+            print(f"x{basis[j]+1}, ", end='')
+        b = np.dot(b, n_b)
+        print("\nTransform basis matrix")
+        print(b, "\n")
+        det = np.linalg.det(b)
+        if(det == 0):
+            print("NO SOLUTION ")
+            break 
+        elif(det != 0):
+            inv_b = np.linalg.inv(b)
+        y = np.dot(cb, inv_b)
+        col = -1
+        mx = 0
+        for i in range(count):
+            if(basic[i]==0):
+                coeff = [[]for k in range(n)]
+                for j in range(n):
+                    coeff[j].append(mat[j][i])
+                z = np.dot(y,coeff)
+                if(mx < obj[i]-z):
+                    mx = obj[i]-z
+                    col = i
+        if(col == -1):
+            ans = 0
+            f = 0
+            for j in range(n):
+                if(obj[basis[j]] == -1*M):
+                    f = 1
+                    break
+    
+            if(f == 1):
+                print("NO SOLUTION ")
+                break
+            elif(f == 0):
+                for j in range(n):
+                    print(f"x{basis[j]+1} = {mat[j][count]}")
+                    if(basis[j] < var):
+                        ans+=(obj[basis[j]]*mat[j][count])
+                print("OPTIMAL VALUE = ", end = '')
+                if(f == 1):
+                    print(-1*ans)
+                else:
+                    print(ans)
+                break
+    
+    
+        n_cff = [[mat[j][col]]for j in range(n)]
+    
+        tmp = np.dot(inv_b, n_cff)
+        rep = [[tmp[j][0]]for j in range(n)]
+    
+        row = -1
+        zm=1e5
+        for j in range(n):
+            if(rep[j][0]>0):
+                if(zm>mat[j][count]/rep[j][0]):
+                    zm = mat[j][count]/rep[j][0]
+                    row = j
+    
+        if(row != -1):
+            for j in range(n):
+                if(j != row):
+                    mat[j][count] -= rep[j][0]*zm
+            mat[row][count] = zm
+        elif(row == -1):
+            print("UNBOUNDED SOLUTION")
+            break
+    
+        basis[row] = col
+    
+        for j in range(count):
+            basic[j] = 0
+        for j in range(n):
+            basic[basis[j]] = 1
+    
+        for j in range(n):
+            for k in range(n):
+                if(j == k):
+                    n_b[j][k] = 1
+                elif(j!=k):
+                    n_b[j][k] = 0
+    
+        for j in range(n):
+            n_b[j][row] = rep[j][0]
+        print("\n------------------------------------------------------------------")
+        itr+=1
+    print("\n------------------------------------------------------------------")
